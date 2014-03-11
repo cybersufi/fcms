7ee89e3e010f7cb2d63baeb44e76c4cd2b6a471d
cybersufi <cybersufi@gmail.com>
2014-03-11 22:39:04 +0700

    initial
    initial

---
+++ fuel/application/cache/dwoo/compiled/07375c48e260af650767ef3c8e084d03.d17.php
@@ -0,0 +1,5 @@
+<?php
+/* template head */
+/* end template head */ ob_start(); /* template body */ ?><!--__FUEL_MARKER__0-->Test Page<?php  /* end template body */
+return $this->buffer . ob_get_clean();
+?>
---
+++ fuel/modules/user_guide/views/_blocks/config.php
@@ -0,0 +1,24 @@
+<?php if ($config) : ?>
+<h2><?=humanize($module)?> Configuration</h2>
+<p>The following configuration parameters can be found in the <dfn>modules/<?=$module?>/config/<?=$module?>.php</dfn> configuration file.</p>
+<table border="0" cellspacing="1" cellpadding="0" class="tableborder">
+	<tbody>
+		<tr>
+			<th>Property</th>
+			<th>Default Value</th>
+			<th>Description</th>
+		</tr>
+	<?php foreach($config as $key => $c) : ?>
+		<tr>
+			<td><strong><?=$key?></strong></td>
+			<td>
+				<?=$c->default_value?>
+			</td>
+			<td><?=$c->comment?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+
+<br />
+<?php endif; ?>
